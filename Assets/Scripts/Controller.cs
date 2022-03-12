ab89a70709b2288a63ef562a49708098b73e12c1
Aybars <aybarsduran@outlook.com>
2022-03-12 21:07:28 +0300

    Updated animations

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,45 +4,88 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    private CharacterController controller;
-  
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
 
-    private void Start()
+    float inputX;
+    float inputY;
+    float normalFov;
+    float sprintFov =70;
+
+    public Transform Model;
+    Animator Anim;
+    Vector3 stickDirection;
+
+    Camera mainCam;
+
+    public float damp;
+
+    [Range(1, 20)]
+    public float rotationSpeed;
+
+    float maxSpeed;
+    public KeyCode sprintBtn = KeyCode.LeftShift;
+    public KeyCode walkBtn = KeyCode.C;
+
+
+    void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        Anim = GetComponent<Animator>();
+        mainCam = Camera.main;
+        normalFov = mainCam.fieldOfView;
+
     }
 
-    void Update()
+    private void LateUpdate()
     {
-        controller.height=1.8f;
-
-        controller.center = new Vector3(0, 1, 0);
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-     
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+       
+        InputMove();
+        InputRotation();
+        Movement();
     }
+    void Movement()
+    {
+        stickDirection = new Vector3(inputX, 0, inputY);
+
+        if (Input.GetKey(sprintBtn))
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, sprintFov, Time.deltaTime * 2);
+
+            maxSpeed = 2;
+            inputX = 2 * Input.GetAxis("Horizontal");
+            inputY = 2 * Input.GetAxis("Vertical");
+
+        }
+        else if (Input.GetKey(walkBtn))
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, Time.deltaTime * 2);
+            maxSpeed = 0.2f;
+            inputX = Input.GetAxis("Horizontal");
+            inputY = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, Time.deltaTime * 2);
+            maxSpeed = 1;
+            inputX = Input.GetAxis("Horizontal");
+            inputY = Input.GetAxis("Vertical");
+
+        }
+      
+    }
+
+    void InputMove()
+    {
+        Anim.SetFloat("speed", Vector3.ClampMagnitude(stickDirection, maxSpeed).magnitude, damp, Time.deltaTime * 10);
+    }
+
+    void InputRotation()
+    {
+
+        Vector3 rotOfset = mainCam.transform.TransformDirection(stickDirection);
+        rotOfset.y = 0;
+
+        Model.forward = Vector3.Slerp(Model.forward, rotOfset, rotationSpeed * Time.deltaTime);
+
+
+    }
+
 }
